1802acc4829e12be3295f20b5f8446717bd33971
karthikjamadagni <narvekarthik@gmail.com>
2023-04-29 14:34:11 +0530

    update

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(bodyParser.json());
 
-const hostname = '0.0.0.0'
 const port = 9002;
 
-
-mongoose.connect('mongodb+srv://movieapp:movie123@karthik.yyvbipq.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://karthikng:karthikng44@cluster0.zpsvxbb.mongodb.net/?retryWrites=true&w=majority',{
+// mongoose.connect('mongodb://localhost:27017/loginsignup',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, ()=>{
@@ -83,18 +82,18 @@ app.post('/fetchfavorites', (req, res)=>{
 });
 });
 
-app.post('/fetchfavorites1', (req, res)=>{
-  const usrid = "63f62325abe358e385209e33";
-  User.findOne({_id:usrid}, (err, user)=>{
-    if(user){
-    res.send({message: user.favorites})
-    // console.log(user.favorites);
-  }
-  else{
-    res.send({message: "No favorites"});
-  }
-});
-});
+// app.post('/fetchfavorites1', (req, res)=>{
+//   const usrid = "63f62325abe358e385209e33";
+//   User.findOne({_id:usrid}, (err, user)=>{
+//     if(user){
+//     res.send({message: user.favorites})
+//     // console.log(user.favorites);
+//   }
+//   else{
+//     res.send({message: "No favorites"});
+//   }
+// });
+// });
 
 
 app.post('/register', (req, res)=>{
@@ -119,12 +118,9 @@ app.post('/register', (req, res)=>{
         }
     })
 })
-app.listen(port, hostname,  ()=>{
-    console.log(`http://${hostname}:${port}`);
-})
+app.listen(port,  ()=>{
+    console.log('API started on port 9002');
+});
 
 
-const getdbfavs = () => {
-        axios.post('http://localhost:9002/fetchfavorites', user)
-        .then((res) => setdbfav(res.data.message))
-    }
+// mongodb+srv://movieapp:movieapp123@karthik.yyvbipq.mongodb.net/?retryWrites=true&w=majority
